6ff0c2d4e628a8cce8879381f30975102c907d1e
whyhugo <hugo.tw.wang@gmail.com>
2023-12-18 03:02:56 +0800

    update models

---
+++ model/LinearRegression.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from scipy import stats
+
+data = pd.read_csv(r'D:\programing\swiftx\NTNU\Data Analysis in Sports\mlb_stats\data.csv')
+
+X = data.drop('RANK', axis=1)
+y = data['RANK']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f'Mean Squared Error: {mse}')
+print(f'Mean Absolute Error: {mae}')
+print(f'R-squared: {r2}')
+
+plt.figure(figsize=(10, 6))
+sns.scatterplot(x=y_test, y=y_pred)
+plt.title('Scatter Plot of True vs Predicted Values')
+plt.xlabel('True Values')
+plt.ylabel('Predicted Values')
+plt.show()
+
+residuals = y_test - y_pred
+plt.figure(figsize=(10, 6))
+sns.residplot(x=y_test, y=residuals, lowess=True, color='g')
+plt.title('Residual Plot')
+plt.xlabel('True Values')
+plt.ylabel('Residuals')
+plt.show()
+
+plt.figure(figsize=(10, 6))
+stats.probplot(residuals, dist="norm", plot=plt)
+plt.title('Q-Q Plot of Residuals')
+plt.show()
--- model/SGDRegressor.py
+++ model/SGDRegressor.py
@@ -3,11 +3,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import explained_variance_score
 
-data = pd.read_csv('your_data.csv')
+data = pd.read_csv(r'D:\programing\swiftx\NTNU\Data Analysis in Sports\mlb_stats\data.csv')
 
-X = data.drop('Rank', axis=1)
-y = data['Rank']
+X = data.drop('RANK', axis=1)
+y = data['RANK']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -22,3 +25,9 @@ predictions = model.predict(X_test_scaled)
 
 mse = mean_squared_error(y_test, predictions)
 print(f'Mean Squared Error: {mse}')
+mae = mean_absolute_error(y_test, predictions)
+print(f'Mean Absolute Error: {mae}')
+r2 = r2_score(y_test, predictions)
+print(f'R^2 Score: {r2}')
+evs = explained_variance_score(y_test, predictions)
+print(f'Explained Variance Score: {evs}')
